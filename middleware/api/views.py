23aa500a4237fd77129e38b07c5234667d231ee3
Gareth <gareth@garethveale.dev>
2019-10-02 08:25:03 +0100

    exposing posts from db via api in the index view func

--- middleware/api/views.py
+++ middleware/api/views.py
@@ -1,12 +1,24 @@
 from api import app
 from api.forms import ContactForm
+from api.models import Post, BookNote
 # from api.email import send_contact_form
 from flask import jsonify
 
 
 @app.route('/')
 def index():
-    posts = ['post1', 'post2']
+    posts = []
+    post_list = Post.query.all()
+
+    for post in post_list:
+        categories = [category.name for category in post.categories]
+        posts.append({
+            'title': post.title,
+            'body': post.body,
+            'categories': categories,
+            'time_posted': post.timestamp
+        })
+
     return jsonify({'posts': posts})
 
 # @app.route('/contact', methods=['GET', 'POST'])
