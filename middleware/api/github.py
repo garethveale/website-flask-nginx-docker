43c6b93063a439dd8edbd09540c8e54eea8c3e4c
Gareth <gareth@garethveale.dev>
2019-10-07 07:09:27 +0100

    github api clean up

--- middleware/api/github.py
+++ middleware/api/github.py
@@ -5,18 +5,7 @@ URL = 'https://api.github.com/'
 
 
 def git_stats(user):
-    ''' Get all github statistics '''
-    user_data = get_user(user)
-    response = get_repositories(user)
-    return response
-
-
-def get_user(user):
-    url = 'users/{}'.format(user)
-    return requests.get(URL + url).json()
-
-
-def get_repositories(user):
+    ''' Get all github repo '''
     repos = {}
     url = 'users/{}/repos'.format(user)
     response = requests.get(URL + url).json()
