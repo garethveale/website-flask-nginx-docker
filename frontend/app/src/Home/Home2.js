3c694e02bedcb76e40c7491746c359ef01bf426a
garethveale <gareth@garethveale.dev>
2019-11-02 09:36:18 +0000

    updated pip req file. Architecture updates and a todo list

--- frontend/app/src/Home/Home2.js
+++ frontend/app/src/Home/Home2.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { HomeHeading } from './HomeHeading';
-import { ThumbnailRow } from './ThumbnailRow';
-import { Footer } from '../common/Footer';
 import { NavBar } from '../common/NavBar';
 
 class Home2 extends React.Component {
